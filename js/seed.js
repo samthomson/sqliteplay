7f1b2626ff77c68588d1859a834fa609c58ee75f
Sam Thomson <samt@samt.st>
2018-12-02 17:33:32 +0700

    db tweeks

--- js/seed.js
+++ js/seed.js
@@ -3,14 +3,14 @@ const { createDB } = require('./create.js')
 var db = createDB()
 
 db.serialize(function() {
-    db.run(`CREATE TABLE files (
+    db.run(`CREATE TABLE IF NOT EXISTS files (
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(32) NOT NULL,
         full_path VARCHAR(128) NOT NULL,
         date_added TIMESTAMP NOT NULL
      )`)
 
-    db.run(`CREATE TABLE tags (
+    db.run(`CREATE TABLE IF NOT EXISTS tags (
         id INTEGER AUTO_INCREMENT PRIMARY KEY,
         file_id INTEGER,
         type VARCHAR(32),
@@ -18,11 +18,16 @@ db.serialize(function() {
         confidence INTEGER
      )`)
    
-    // var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    // for (var i = 0; i < 10; i++) {
-    //     stmt.run("Ipsum " + i);
-    // }
-    // stmt.finalize();
+    var stmt = db.prepare("INSERT INTO files (name, full_path, date_added) VALUES (?, ?, datetime())");
+    for (var i = 0; i < 10; i++) {
+        stmt.run(
+            [
+                `file_number_${i}`,
+                `~/full/file/path/file_number_${i}.jpg`,
+            ]
+        );
+    }
+    stmt.finalize();
    
     // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
     //     console.log(row.id + ": " + row.info);
